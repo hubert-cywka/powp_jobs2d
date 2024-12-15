fedb8c19fd8d9f662abf88d1ef14364a58403242
Hubert Cywka <hcywka@gmail.com>
2024-12-15 10:32:24 +0100

    Add DriverCommand hierarchy

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -0,0 +1,19 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
+    private final Job2dDriver driver;
+    private final int x;
+    private final int y;
+
+    public OperateToCommand(Job2dDriver driver, int x, int y) {
+        this.driver = driver;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void execute() {
+        this.driver.operateTo(this.x, this.y);
+    }
+}
